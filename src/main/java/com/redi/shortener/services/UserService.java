3d0a030b5a3b33b4e7222da075fae732376ed766
Kim Covaci <kimk@spotify.com>
2022-11-02 20:13:45 +0100

    Add email check

--- src/main/java/com/redi/shortener/services/UserService.java
+++ src/main/java/com/redi/shortener/services/UserService.java
@@ -2,6 +2,7 @@ package com.redi.shortener.services;
 
 import com.redi.shortener.model.User;
 import com.redi.shortener.model.UserRegistration;
+
 import java.util.Map;
 import java.util.UUID;
 import java.util.concurrent.ConcurrentHashMap;
@@ -10,16 +11,19 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class UserService {
-  private final Map<UUID, User> users = new ConcurrentHashMap<>();
+    private final Map<UUID, User> users = new ConcurrentHashMap<>();
 
-  public User create(final UserRegistration registration) {
-    final UUID id = UUID.randomUUID();
-    final User user = new User(id, registration.name(), registration.email());
-    users.put(id, user);
-    return user;
-  }
+    public User create(final UserRegistration registration) {
+        final UUID id = UUID.randomUUID();
+        if (users.values().stream().anyMatch(user -> user.email().equals(registration.email()))) {
+            throw new RuntimeException("User already exists:" + id);
+        }
+        final User user = new User(id, registration.name(), registration.email());
+        users.put(id, user);
+        return user;
+    }
 
-  public User get(final UUID userId) {
-    return users.get(userId);
-  }
+    public User get(final UUID userId) {
+        return users.get(userId);
+    }
 }
